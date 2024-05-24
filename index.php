d685dd1a997934e367b5e69ab22a7e79ee2ced65
Brian Thomas <56766194+brianbct129@users.noreply.github.com>
2024-05-24 19:13:59 +0800

    feat: relayout susunan index.php

--- index.php
+++ index.php
@@ -372,50 +372,14 @@
 					</div>
 				</div>
 				
-				<!-- Testimonial Block -->
-				<div class="testimonial-block">
-					<div class="inner-box">
-						<div class="quote icofont-quote-right"></div>
-						<div class="author">Ryan Betthalyn <span>/ Director at Chobham Manor</span></div>
-						<div class="text">Sed elit quam, iaculis sed semper sit amet udin vitae nibh. Rubino at magna akal semperFusce commodo molestie luctus.Lorem ipsum Dolor tusima olatiup.</div>
-					</div>
-				</div>
 				
-				<!-- Testimonial Block -->
-				<div class="testimonial-block">
-					<div class="inner-box">
-						<div class="quote icofont-quote-right"></div>
-						<div class="author">Bobs Hanley <span>/ Director at Spotify</span></div>
-						<div class="text">Sed elit quam, iaculis sed semper sit amet udin vitae nibh. Rubino at magna akal semperFusce commodo molestie luctus.Lorem ipsum Dolor tusima olatiup.</div>
-					</div>
-				</div>
 				
 			</div>
 		</div>
 	</div>
 	<!-- End Testimonial Section -->
 
-	<!-- Sponsors Section -->
-	<div class="sponsors-section">
-		<div class="auto-container">
-			
-			<div class="carousel-outer">
-                <!--Sponsors Slider-->
-                <ul class="sponsors-carousel owl-carousel owl-theme">
-                    <li><div class="image-box"><a href="#"><img src="images/clients/1.png" alt=""></a></div></li>
-                    <li><div class="image-box"><a href="#"><img src="images/clients/2.png" alt=""></a></div></li>
-                    <li><div class="image-box"><a href="#"><img src="images/clients/3.png" alt=""></a></div></li>
-                    <li><div class="image-box"><a href="#"><img src="images/clients/4.png" alt=""></a></div></li>
-					<li><div class="image-box"><a href="#"><img src="images/clients/5.png" alt=""></a></div></li>
-                    <li><div class="image-box"><a href="#"><img src="images/clients/1.png" alt=""></a></div></li>
-                    <li><div class="image-box"><a href="#"><img src="images/clients/2.png" alt=""></a></div></li>
-                    <li><div class="image-box"><a href="#"><img src="images/clients/3.png" alt=""></a></div></li>
-                </ul>
-            </div>
-			
-		</div>
-	</div>
-	<!--End Sponsors Section-->
+	
 	
 	<!-- Reputation Section Two -->
 	<div class="reputation-section-two style-two">
@@ -527,69 +491,28 @@
 				</div>
 			</div>
 			
-			<div class="row clearfix">
-				
-				<!-- Team Block -->
-				<div class="team-block col-lg-4 col-md-6 col-sm-6 col-12">
-					<div class="inner-box wow fadeInLeft" data-wow-delay="0ms" data-wow-duration="1500ms">
-						<div class="image">
-							<a href="#"><img src="images/resource/team-1.jpg" alt="" /></a>
-							<!-- Social Box -->
-							<ul class="social-box">
-								<li><a href="https://twitter.com/" class="icofont-twitter"></a></li>
-								<li><a href="http://facebook.com/" class="icofont-facebook"></a></li>
-								<li><a href="https://www.instagram.com/" class="icofont-instagram"></a></li>
-								<li><a href="https://www.linkedin.com/" class="icofont-linkedin"></a></li>
-							</ul>
-						</div>
-						<div class="lower-box mt-0">
-							<h4><a href="#">Edward Eric Jr</a></h4>
-							<div class="designation">Business & Financial Expert</div>
-						</div>
+			<!-- Sponsors Section -->
+			<div class="sponsors-section">
+			<div class="auto-container">
+					
+					<div class="carousel-outer">
+						<!--Sponsors Slider-->
+						<ul class="sponsors-carousel owl-carousel owl-theme">
+							<li><div class="image-box"><a href="#"><img src="images/clients/1.png" alt=""></a></div></li>
+							<li><div class="image-box"><a href="#"><img src="images/clients/2.png" alt=""></a></div></li>
+							<li><div class="image-box"><a href="#"><img src="images/clients/3.png" alt=""></a></div></li>
+							<li><div class="image-box"><a href="#"><img src="images/clients/4.png" alt=""></a></div></li>
+							<li><div class="image-box"><a href="#"><img src="images/clients/5.png" alt=""></a></div></li>
+							<li><div class="image-box"><a href="#"><img src="images/clients/1.png" alt=""></a></div></li>
+							<li><div class="image-box"><a href="#"><img src="images/clients/2.png" alt=""></a></div></li>
+							<li><div class="image-box"><a href="#"><img src="images/clients/3.png" alt=""></a></div></li>
+						</ul>
 					</div>
+					
 				</div>
-				
-				<!-- Team Block -->
-				<div class="team-block col-lg-4 col-md-6 col-sm-6 col-12">
-					<div class="inner-box wow fadeInUp" data-wow-delay="0ms" data-wow-duration="1500ms">
-						<div class="image">
-							<a href="#"><img src="images/resource/team-2.jpg" alt="" /></a>
-							<!-- Social Box -->
-							<ul class="social-box">
-								<li><a href="https://twitter.com/" class="icofont-twitter"></a></li>
-								<li><a href="http://facebook.com/" class="icofont-facebook"></a></li>
-								<li><a href="https://www.instagram.com/" class="icofont-instagram"></a></li>
-								<li><a href="https://www.linkedin.com/" class="icofont-linkedin"></a></li>
-							</ul>
-						</div>
-						<div class="lower-box mt-0">
-							<h4><a href="#">Tom Holland</a></h4>
-							<div class="designation">Logistic & Communication Expert</div>
-						</div>
-					</div>
-				</div>
-				
-				<!-- Team Block -->
-				<div class="team-block col-lg-4 col-md-6 col-sm-6 col-12">
-					<div class="inner-box wow fadeInRight" data-wow-delay="0ms" data-wow-duration="1500ms">
-						<div class="image">
-							<a href="#"><img src="images/resource/team-3.jpg" alt="" /></a>
-							<!-- Social Box -->
-							<ul class="social-box">
-								<li><a href="https://twitter.com/" class="icofont-twitter"></a></li>
-								<li><a href="http://facebook.com/" class="icofont-facebook"></a></li>
-								<li><a href="https://www.instagram.com/" class="icofont-instagram"></a></li>
-								<li><a href="https://www.linkedin.com/" class="icofont-linkedin"></a></li>
-							</ul>
-						</div>
-						<div class="lower-box mt-0">
-							<h4><a href="#">Laura Erakovic</a></h4>
-							<div class="designation">Consumer Market Expert</div>
-						</div>
-					</div>
-				</div>
-				
 			</div>
+			<!--End Sponsors Section-->
+			
 		</div>
 	</div>
 	<!-- End Experts Section -->
