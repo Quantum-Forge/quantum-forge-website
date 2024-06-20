ef971159a68ce96216121bbd514eb16b3049b29e
Brian Thomas <56766194+brianbct129@users.noreply.github.com>
2024-06-20 10:38:12 +0800

    feat: selesai dengan pembuatan halaman tentang

--- about-1.php
+++ about-1.php
@@ -44,7 +44,7 @@
     <!--End Main Header -->
 	
 	<!-- Service Banner Section -->
-	<div class="service-banner-section" data-bg-image="images/background/intro-1.png">
+	<div class="service-banner-section" data-bg-image="images/background/intro-2.png">
 		<div class="auto-container">
 			<div class="content-box">
 				<h2>Pemimpin <span>Software House</span> di <span>Indonesia</span></h2>
@@ -92,7 +92,11 @@
 								<h2><span>Visi</span> Kami</h2>
 							</div>
 							<div class="text">
-								<p>Menjadi pemimpin global dalam pengembangan perangkat lunak, menginspirasi inovasi, dan mendorong transformasi digital di seluruh dunia.</p>
+								<p>
+									<ul class="list-style-one">
+										<li>Menjadi pemimpin global dalam pengembangan perangkat lunak, menginspirasi inovasi, dan mendorong transformasi digital di seluruh dunia.</li>
+									</ul>
+								</p>
 							</div>
 						</div>
 					</div>
@@ -123,6 +127,34 @@
 							</div>
 						</div>
 					</div>
+
+					<!-- Image Column -->
+					<div class="image-column col-lg-6">
+						<div class="about-image">
+							<div class="about-inner-image">
+								<img src="images/about/1.png" alt="about">
+							</div>
+						</div>
+					</div>
+
+					<!-- Content Column -->
+					<div class="content-column col-lg-6 col-md-12 col-sm-12 mb-0">
+						<div class="inner-column">
+							<div class="sec-title">
+								<h2><span>Nilai</span> Kami</h2>
+							</div>
+							<div class="text">
+								<p>
+									<ul class="list-style-one">
+										<li><b>Inovasi</b>: Kami selalu mencari cara baru dan lebih baik untuk menyelesaikan masalah</li>
+										<li><b>Kualitas</b>: Kami berkomitmen untuk menyediakan produk dan layanan yang memenuhi standar tertinggi.</li>
+										<li><b>Kolaborasi</b>: Kami percaya bahwa kerjasama adalah kunci keberhasilan.</li>
+										<li><b>Integritas</b>: Kami menjalankan bisnis kami dengan transparansi dan kejujuran.</li>
+									</ul>
+								</p>
+							</div>
+						</div>
+					</div>
 					
 				</div>
 			</div>
@@ -130,223 +162,48 @@
 	</div>
 	<!-- End About Section Two -->
 
-	
-	
-	<!-- Services Section -->
-	<div class="services-section section-padding">
-		<div class="auto-container">
-			<div class="sec-title">
-				<div class="title">our services</div>
-				<h2><span>Best Solutions</span> For Your Business</h2>
-			</div>
-			<div class="inner-container">
-				<div class="row g-0">
-				
-					<!-- Service Block -->
-					<div class="service-block col-lg-3 col-md-6 col-sm-12">
-						<div class="inner-box">
-							<div class="icon-box">
-								<span class="icon ti-blackboard"></span>
-							</div>
-							<h5><a href="service-detail.php">Strategy</a></h5>
-							<div class="text">Thought leadership pass the clap hackathon wearables.</div>
-							<a class="read-more" href="service-detail.php">More <span class="ti-angle-right"></span></a>
-						</div>
-					</div>
-					
-					<!-- Service Block -->
-					<div class="service-block col-lg-3 col-md-6 col-sm-12">
-						<div class="inner-box">
-							<div class="icon-box">
-								<span class="icon ti-stats-up"></span>
-							</div>
-							<h5><a href="service-detail.php">Financial</a></h5>
-							<div class="text">Platform omnichannel click thought leadership pivot</div>
-							<a class="read-more" href="service-detail.php">More <span class="ti-angle-right"></span></a>
-						</div>
-					</div>
-					
-					<!-- Service Block -->
-					<div class="service-block col-lg-3 col-md-6 col-sm-12">
-						<div class="inner-box">
-							<div class="icon-box">
-								<span class="icon ti-shield"></span>
-							</div>
-							<h5><a href="service-detail.php">Insurance</a></h5>
-							<div class="text">Disrupt taste makers help council emerging.</div>
-							<a class="read-more" href="service-detail.php">More <span class="ti-angle-right"></span></a>
-						</div>
-					</div>
-					
-					<!-- Service Block -->
-					<div class="service-block col-lg-3 col-md-6 col-sm-12">
-						<div class="inner-box">
-							<div class="icon-box">
-								<span class="icon ti-home"></span>
-							</div>
-							<h5><a href="service-detail.php">Estate</a></h5>
-							<div class="text">Mobile ready proprietary dedication intuitive</div>
-							<a class="read-more" href="service-detail.php">More <span class="ti-angle-right"></span></a>
-						</div>
-					</div>
-					
-					<!-- Service Block -->
-					<div class="service-block col-lg-3 col-md-6 col-sm-12">
-						<div class="inner-box">
-							<div class="icon-box">
-								<span class="icon ti-stamp"></span>
-							</div>
-							<h5><a href="service-detail.php">Audit & Evaluation</a></h5>
-							<div class="text">Thought leadership pass the clap hackathon wearables.</div>
-							<a class="read-more" href="service-detail.php">More <span class="ti-angle-right"></span></a>
-						</div>
-					</div>
-					
-					<!-- Service Block -->
-					<div class="service-block col-lg-3 col-md-6 col-sm-12">
-						<div class="inner-box">
-							<div class="icon-box">
-								<span class="icon ti-money"></span>
-							</div>
-							<h5><a href="service-detail.php">Taxes & Efficiency</a></h5>
-							<div class="text">Disrupt taste makers help council emerging.</div>
-							<a class="read-more" href="service-detail.php">More <span class="ti-angle-right"></span></a>
-						</div>
-					</div>
-					
-					<!-- Service Block -->
-					<div class="service-block col-lg-3 col-md-6 col-sm-12">
-						<div class="inner-box">
-							<div class="icon-box">
-								<span class="icon ti-shopping-cart-full"></span>
-							</div>
-							<h5><a href="service-detail.php">Consumer Market</a></h5>
-							<div class="text">Mobile ready proprietary dedication intuitive</div>
-							<a class="read-more" href="service-detail.php">More <span class="ti-angle-right"></span></a>
-						</div>
-					</div>
-					
-					<!-- Service Block -->
-					<div class="service-block col-lg-3 col-md-6 col-sm-12">
-						<div class="inner-box">
-							<div class="icon-box">
-								<span class="icon ti-truck"></span>
-							</div>
-							<h5><a href="service-detail.php">Logistic</a></h5>
-							<div class="text">Platform omnichannel click thought leadership pivot</div>
-							<a class="read-more" href="service-detail.php">More <span class="ti-angle-right"></span></a>
-						</div>
-					</div>
-					
-				</div>
-				
-			</div>
-		</div>
-	</div>
-	<!-- End Services Section -->
-	
-	<!-- Reputation Section Two -->
-	<div class="reputation-section-two style-two">
-		<div class="auto-container">
-			<div class="row clearfix">
-				
-				<!-- Form Column -->
-				<div class="form-column col-lg-5 col-md-12 col-sm-12">
-					<div class="inner-column">
-						<div class="form-boxed">
-							<h5>free consulation</h5>
-							
-							<div class="consult-form">
-								<form method="post" action="donate.php">
-
-									<!--Form Group-->
-									<div class="form-group">
-										<label>full name</label>
-										<input type="text" name="name" value="" placeholder="Jason Staham" required>
-									</div>
-									<!--Form Group-->
-									<div class="form-group">
-										<label>phone number</label>
-										<input type="text" name="phone" value="" placeholder="Enter your phone number" required>
-									</div>
-									<!--Form Group-->
-									<div class="form-group">
-										<label>topics</label>
-										<select class="custom-select-box">
-											<option>Choose topic</option>
-											<option>Topic 01</option>
-											<option>Topic 02</option>
-											<option>Topic 03</option>
-											<option>Topic 04</option>
-										</select>
-									</div>
-									<!--Form Group-->
-									<div class="form-group">
-										<label>message</label>
-										<textarea name="message" placeholder="Write your message here"></textarea>
-									</div>
-									<div class="form-group">
-										<button class="theme-btn btn-style-one" type="submit" name="submit-form"><span class="txt">send your messenger</span></button>
-									</div>
-								</form>
-							
-							</div>
-						</div>
-						
-					</div>
-				</div>
-				
-				<!-- Content Column -->
-				<div class="content-column col-lg-7 col-md-12 col-sm-12">
-					<div class="inner-column">
-						<div class="sec-title">
-							<div class="title">why choose us</div>
-							<h2><span>Your Successful </span>Is<br> Our Reputation</h2>
-						</div>
-						<div class="blocks-outer">
-							
-							<!-- Reputation Block -->
-							<div class="reputation-block">
-								<div class="inner-box">
-									<h5>Strong Martket Analysis</h5>
-									<div class="text">Tasks, docs, and files integrate with Arado’s chat to close the <br> gaps between feedback and action.</div>
-								</div>
-							</div>
-							
-							<!-- Reputation Block -->
-							<div class="reputation-block">
-								<div class="inner-box">
-									<h5>Experience & Percision</h5>
-									<div class="text">Create multiple discussions to keep all relevant conversations <br> together, all in one place with Arado</div>
-								</div>
-							</div>
-							
-							<!-- Reputation Block -->
-							<div class="reputation-block">
-								<div class="inner-box">
-									<h5>Experts About Business</h5>
-									<div class="text">Create multiple discussions to keep all relevant conversations <br> together, all in one place with Arado</div>
-								</div>
-							</div>
-							
-						</div>
-					</div>
-				</div>
-				
-			</div>
-		</div>
-	</div>
-	<!-- End Reputation Section -->
+	<?php include('layout/free_consultation.php') ?>
 	
 	<!-- Testimonial Section -->
 	<div class="testimonial-section">
 		<div class="auto-container">
 			<div class="inner-container">
+
+				<div class="row align-items-center clearfix mb-4">
+					<!-- Image Column -->
+					<div class="image-column col-lg-6">
+						<div class="about-image">
+							<div class="about-inner-image">
+								<img src="images/about/4.png" alt="about">
+							</div>
+						</div>
+					</div>
+					
+					<!-- Content Column -->
+					<div class="content-column col-lg-6 col-md-12 col-sm-12 mb-0">
+						<div class="inner-column">
+							<div class="sec-title">
+								<h2><span>Proses</span> Kami</h2>
+							</div>
+							<div class="text">
+								<p>
+									<ul class="list-style-one">
+										<li><b>Analisis Kebutuhan</b>: Memahami kebutuhan dan tujuan bisnis klien.</li>
+										<li><b>Perencanaan</b>: Merencanakan solusi yang tepat dan menetapkan jadwal proyek.</li>
+										<li><b>Pengembangan</b>: Mengembangkan perangkat lunak dengan menggunakan metodologi terbaik.</li>
+										<li><b>Pengujian</b>: Melakukan pengujian untuk memastikan kualitas dan fungsionalitas.</li>
+										<li><b>Peluncuran</b>: Meluncurkan produk dan memberikan dukungan pasca peluncuran.</li>
+									</ul>
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
 			
 				<!-- Sec Title -->
 				<div class="sec-title">
-					<div class="title">testimonials</div>
-					<h2><span>+2,500 Clients </span>Love Us</h2>
+					<div class="title">testimoni</div>
+					<h2><span>Kami Peduli </span> dengan Klien kita</h2>
 				</div>
 				<div class="testimonial-carousel owl-carousel owl-theme">
 					
@@ -354,8 +211,8 @@
 					<div class="testimonial-block">
 						<div class="inner-box">
 							<div class="quote icofont-quote-right"></div>
-							<div class="author">Ryan Betthalyn <span>/ Director at Chobham Manor</span></div>
-							<div class="text">Sed elit quam, iaculis sed semper sit amet udin vitae nibh. Rubino at magna akal semperFusce commodo molestie luctus.Lorem ipsum Dolor tusima olatiup.</div>
+							<div class="author">Mr A <span>/ Yayasan Buahatiku</span></div>
+							<div class="text">Quantum Forge telah membantu kami mencapai efisiensi operasional yang lebih tinggi dengan solusi perangkat lunak mereka yang inovatif.</div>
 						</div>
 					</div>
 					
@@ -363,26 +220,8 @@
 					<div class="testimonial-block">
 						<div class="inner-box">
 							<div class="quote icofont-quote-right"></div>
-							<div class="author">Bobs Hanley <span>/ Director at Spotify</span></div>
-							<div class="text">Sed elit quam, iaculis sed semper sit amet udin vitae nibh. Rubino at magna akal semperFusce commodo molestie luctus.Lorem ipsum Dolor tusima olatiup.</div>
-						</div>
-					</div>
-					
-					<!-- Testimonial Block -->
-					<div class="testimonial-block">
-						<div class="inner-box">
-							<div class="quote icofont-quote-right"></div>
-							<div class="author">Ryan Betthalyn <span>/ Director at Chobham Manor</span></div>
-							<div class="text">Sed elit quam, iaculis sed semper sit amet udin vitae nibh. Rubino at magna akal semperFusce commodo molestie luctus.Lorem ipsum Dolor tusima olatiup.</div>
-						</div>
-					</div>
-					
-					<!-- Testimonial Block -->
-					<div class="testimonial-block">
-						<div class="inner-box">
-							<div class="quote icofont-quote-right"></div>
-							<div class="author">Bobs Hanley <span>/ Director at Spotify</span></div>
-							<div class="text">Sed elit quam, iaculis sed semper sit amet udin vitae nibh. Rubino at magna akal semperFusce commodo molestie luctus.Lorem ipsum Dolor tusima olatiup.</div>
+							<div class="author">Mr B <span>/ PT Siagamedika Abadi Karya</span></div>
+							<div class="text">Kerja sama dengan Quantum Forge sangat menyenangkan. Mereka selalu memenuhi tenggat waktu dan memberikan hasil yang luar biasa.</div>
 						</div>
 					</div>
 					
@@ -395,25 +234,20 @@
 	
 	<!-- Sponsors Section -->
 	<div class="sponsors-section">
-		<div class="auto-container">
-			
-			<div class="carousel-outer">
-                <!--Sponsors Slider-->
-                <ul class="sponsors-carousel owl-carousel owl-theme">
-                    <li><div class="image-box"><a href="#"><img src="images/clients/1.png" alt=""></a></div></li>
-                    <li><div class="image-box"><a href="#"><img src="images/clients/2.png" alt=""></a></div></li>
-                    <li><div class="image-box"><a href="#"><img src="images/clients/3.png" alt=""></a></div></li>
-                    <li><div class="image-box"><a href="#"><img src="images/clients/4.png" alt=""></a></div></li>
-					<li><div class="image-box"><a href="#"><img src="images/clients/5.png" alt=""></a></div></li>
-                    <li><div class="image-box"><a href="#"><img src="images/clients/1.png" alt=""></a></div></li>
-                    <li><div class="image-box"><a href="#"><img src="images/clients/2.png" alt=""></a></div></li>
-                    <li><div class="image-box"><a href="#"><img src="images/clients/3.png" alt=""></a></div></li>
-                </ul>
-            </div>
-			
-		</div>
-	</div>
-	<!--End Sponsors Section-->
+			<div class="auto-container">
+					<div class="carousel-outer">
+						<!--Sponsors Slider-->
+						<ul class="sponsors-carousel owl-carousel owl-theme">
+							<li><div class="image-box"><a href="#"><img src="images/provider/1.png" alt=""></a></div></li>
+							<li><div class="image-box"><a href="#"><img src="images/provider/2.png" alt=""></a></div></li>
+							<li><div class="image-box"><a href="#"><img src="images/provider/3.png" alt=""></a></div></li>
+							<li><div class="image-box"><a href="#"><img src="images/provider/4.png" alt=""></a></div></li>
+							<li><div class="image-box"><a href="#"><img src="images/provider/5.png" alt=""></a></div></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<!--End Sponsors Section-->
 	
 	<!-- Main Footer -->
     <footer class="main-footer style-two">
