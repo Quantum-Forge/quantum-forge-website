3e5d404649f5289174899175371b2c40fab2ba47
Brian Thomas <56766194+brianbct129@users.noreply.github.com>
2025-10-27 11:56:49 +0700

    fix(views): add missing width and height attributes to portfolio images
    Ensure proper image dimensions are specified to prevent layout shifts during loading

--- resources/views/details/portfolio.blade.php
+++ resources/views/details/portfolio.blade.php
@@ -111,12 +111,12 @@
                     </div>
                     <div class="col-lg-6 col-md-6 col-12 mt-50">
                         <div class="thumbnail">
-                            <img class="w-100" src="{{ $portfolio->images3_url }}" alt="finance">
+                            <img class="w-100" width="573" height="614" src="{{ $portfolio->images3_url }}" alt="finance">
                         </div>
                     </div>
                     <div class="col-lg-12 mtb-30">
                         <div class="thumbnail">
-                            <img class="w-100" src="{{ $portfolio->images4_url }}" alt="finance">
+                            <img class="w-100" width="573" height="614" src="{{ $portfolio->images4_url }}" alt="finance">
                         </div>
                     </div>
                 </div>
